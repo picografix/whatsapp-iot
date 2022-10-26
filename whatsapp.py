f47be3177163ea09618195a30adeb628242b5114
Gauransh <picografix@gmail.com>
2022-10-27 00:18:45 +0530

    particular msg sender

--- whatsapp.py
+++ whatsapp.py
@@ -1,13 +1,26 @@
 from twilio.rest import Client 
- 
-account_sid = 'ACaa40d5fd76e3d54bdde5807fce65adbb' 
-auth_token = '[AuthToken]' 
+import json
+
+f = open('auth.json')
+data = json.load(f)
+account_sid = data["account_sid"]
+auth_token = data["auth_token"]
+
+
 client = Client(account_sid, auth_token) 
- 
-message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body='Kya haal hai bhai ?',      
-                              to='whatsapp:+919462447291' 
-                          ) 
- 
-print(message.sid)
+
+def send_location(number, lat, long, body):
+
+    # client = Client(account_sid, auth_token) 
+    # number =  "+91"+ number
+    # 37.787890,-122.391664
+    message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body=body,
+            persistent_action=[f'geo:{lat},{long}'],
+            to=f'whatsapp:+91{number}'
+        )
+    print(message.sid)
+    return True
+
+send_location("9462447291", "37.787890","-122.391664","Ice cream vendor")

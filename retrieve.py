da0ae001640411734bf17c3a403d0719a5350b7e
Gauransh <picografix@gmail.com>
2022-11-29 15:27:17 +0530

    changes to retrieve

--- retrieve.py
+++ retrieve.py
@@ -5,28 +5,81 @@ from firebase_admin import firestore
 # Use a service account.
 class ReadData():
     def __init__(self) -> None:
-        
+
         self.cred = credentials.Certificate('key.json')
 
         self.app = firebase_admin.initialize_app(self.cred)
 
         self.db = firestore.client()
 
-    def getDict(self):
-        users_ref = self.db.collection('users')
+    def getDict(self,db_id='clients'):
+        users_ref = self.db.collection(db_id)
         docs = users_ref.stream()
         for doc in docs:
             print(f'{doc.id} => {doc.to_dict()}')
 
-    def addData(self, id, desc, latitude, longitude, status):
-        
-        doc_ref = self.db.collection('users').document(id)
+    def registerClient(self, id, name, desc, latitude, longitude, status,short_desc=0,db_id='clients'):
+
+        if(short_desc==0):
+            short_desc = " ".join(desc.split(" ")[-2:])
+
+
+        doc_ref = self.db.collection(db_id).document(id)
         doc_ref.set({
+            'name':name,
             'desc': desc,
+            'short_desc':short_desc,
             'latitude': latitude,
             'longitude': longitude,
             'status': status
         })
+
+    def listRegisteredVendors(self,db_id='clients'):
+        users_ref = self.db.collection(db_id)
+        docs = users_ref.stream()
+        a = []
+        for doc in docs:
+            temp_dict = doc.to_dict()
+            temp = (doc.id,temp_dict.get('name'),temp_dict.get('short_desc'))
+            a.append(temp)
+        return a
+
+
+    def getVendor(self,id,db_id='clients'):
+        doc_ref = self.db.collection(db_id).document(id)
+
+        doc = doc_ref.get()
+
+        if doc.exists:
+            return doc.to_dict()
+        else:
+            print('No registered vendor for this id')
+
+            return None
+
+    def getLocationVendor(self,id,db_id='clients'):
+        doc_ref = self.db.collection(db_id).document(id)
+
+        doc = doc_ref.get()
+
+        if doc.exists:
+            temp_dict = doc.to_dict()
+            return (temp_dict.get('name'),temp_dict.get('latitude'), temp_dict.get('longitude'))
+        else:
+            print('No registered vendor for this id')
+            return None
+
+    def registerUser(self, mobile, latitude, longitude, db_id='user'):
+
+        doc_ref = self.db.collection(db_id).document(mobile)
+        doc_ref.set({
+            'latitude': latitude,
+            'longitude': longitude,
+            'registered_vendors': [],
+        })
+
+    
+
 
 
 
--- test.py
+++ test.py
@@ -5,4 +5,14 @@ rd = ReadData()
 # rd.addData("1011","I am a ice cream vendor",70.655,-60.66,True)
 # rd.addData("1012","I am a pani puri vendor",70.655,-60.66,True)
 # rd.addData("1013","I am a handicrafts vendor",70.655,-60.66,True)
+
+
+rd.registerClient("1011","Atharva","I am a handicrafts vendor",70.655,-60.66,True)
+rd.registerClient("1012","Gauransh","I am panipuri vendor",70.655,-60.66,True)
+rd.registerClient("1013","Aryan","I am a kitchenitem vendor",70.655,-60.66,True)
 rd.getDict()
+
+# print(rd.listRegisteredVendors())
+# print(rd.getVendor('1010'))
+
+print(rd.getLocationVendor('1011'))
